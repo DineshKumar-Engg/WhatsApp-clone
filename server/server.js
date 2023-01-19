58227eda8e6b2896adf26f9a23f19cd50a64e321
DineshKumar-Engg <Dinesh@#1036>
2023-01-20 00:41:17 +0530

    n

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ dotenv.config()
 //
 
 const app =express();
-app.use(cors({origin:["http://localhost:4000","https://whatsapp-clone.onrender.com"]}));
+app.use(cors({origin:["http://localhost:4000","https://whatsapp-clone-70zf.onrender.com"]}));
 app.use(express.json())
 
 const pusher = new Pusher({
